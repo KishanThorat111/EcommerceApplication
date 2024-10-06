e47f67179f43120ecd78a41b46ed3f71a40f0781
KishanThorat111 <KishanThorat111@gmail.com>
2024-10-06 12:05:52 +0100

    updared gaurd

--- cookie-server/middleware/is-authenticated.js
+++ cookie-server/middleware/is-authenticated.js
@@ -12,7 +12,7 @@
 
 ///////////
 const isAuthenticated = (req, res, next) => {
-  if (req.session && req.session.user && req.session.user.role) { // Added role check
+  if (req.session && req.session.user) { 
     return next();
   } else {
     res.status(401).send({ message: "You must be logged in to access this" });
