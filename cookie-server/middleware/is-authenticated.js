d282858331e0389e00625ed40693f3f78ebe2052
KishanThorat111 <KishanThorat111@gmail.com>
2024-10-07 15:22:10 +0100

    without session authentication

--- cookie-server/middleware/is-authenticated.js
+++ cookie-server/middleware/is-authenticated.js
@@ -1,16 +1,3 @@
-
-// // Authentication middleware in CommonJS syntax
-// const isAuthenticated = (req, res, next) => {
-//     if (req.session && req.session.user) {
-//       return next();
-//     } else {
-//       res.status(401).send({ message: "You must be logged in to access this" });
-//     }
-//   };
-  
-//   module.exports = isAuthenticated;
-
-///////////
 const isAuthenticated = (req, res, next) => {
   if (req.session && req.session.user) { 
     return next();
