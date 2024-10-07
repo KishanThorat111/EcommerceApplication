e1a7a125de38db1b913869f4bba548c4d1138654
KishanThorat111 <KishanThorat111@gmail.com>
2024-10-07 15:32:25 +0100

    with session authentication

--- cookie-client/src/app/guards/auth.guard.ts
+++ cookie-client/src/app/guards/auth.guard.ts
@@ -1,35 +1,34 @@
-////////////////////////////
-// import { CanActivateFn, Router } from '@angular/router';
-// import { AuthService } from '../services/auth.service';
-// import { inject } from '@angular/core';
-// import { catchError, map, of } from 'rxjs';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+import { catchError, map, of } from 'rxjs';
 
-// export const authGuard: CanActivateFn = (route, state) => {
-//   const authService = inject(AuthService) as AuthService;
-//   const router = inject(Router) as Router;
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService) as AuthService;
+  const router = inject(Router) as Router;
 
-//   if (route.routeConfig?.path === 'auth') {
-//     return authService.isAuthenticated().pipe(
-//       map(() => {
-//         router.navigate(['/home']);
-//         return false;
-//       }),
-//       catchError(() => {
-//         return of(true);
-//       })
-//     );
-//   }
+  if (route.routeConfig?.path === 'auth') {
+    return authService.isAuthenticated().pipe(
+      map(() => {
+        router.navigate(['/home']);
+        return false;
+      }),
+      catchError(() => {
+        return of(true);
+      })
+    );
+  }
 
-//   return authService.isAuthenticated().pipe(
-//     map(() => {
-//       return true;
-//     }),
-//     catchError(() => {
-//       router.navigate(['/auth']);
-//       return of(false);
-//     })
-//   );
-// };
+  return authService.isAuthenticated().pipe(
+    map(() => {
+      return true;
+    }),
+    catchError(() => {
+      router.navigate(['/auth']);
+      return of(false);
+    })
+  );
+};
 
 // import { CanActivateFn, Router } from '@angular/router';
 // import { AuthService } from '../services/auth.service';
@@ -112,32 +111,5 @@
 //   );
 // };
 
-
-
-
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { inject } from '@angular/core';
-import { catchError, map, of } from 'rxjs';
-
-export const authGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthService) as AuthService;
-  const router = inject(Router) as Router;
-
-  return authService.isAuthenticated().pipe(
-    map((isAuthenticated) => {
-      if (isAuthenticated) {
-        return true; // If authenticated, allow access
-      } else {
-        router.navigate(['/auth']); // Redirect to login if not authenticated
-        return false;
-      }
-    }),
-    catchError(() => {
-      router.navigate(['/auth']); // Redirect to login on error
-      return of(false);
-    })
-  );
-};
 
 
