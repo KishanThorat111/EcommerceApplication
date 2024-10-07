ea89f207893f606c0bd84ed21d377ccf640d6a04
KishanThorat111 <KishanThorat111@gmail.com>
2024-10-07 23:03:01 +0100

    backend

--- cookie-client/src/app/services/api.service.ts
+++ cookie-client/src/app/services/api.service.ts
@@ -66,8 +66,8 @@ import { Observable } from 'rxjs';
 export class ApiService {
   constructor(private httpClient: HttpClient) {}
 
-  // baseUrl = 'https://ecommerceapplication-backend.onrender.com';
-  baseUrl = 'http://localhost:3000';
+  baseUrl = 'https://ecommerceapplication-backend.onrender.com';
+  // baseUrl = 'http://localhost:3000';
 
   post(url: string, body: any): Observable<any> {
     return this.httpClient.post(`${this.baseUrl}${url}`, body, {
